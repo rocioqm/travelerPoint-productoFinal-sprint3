38a521cf90fc483ac27a4629bebad9552927b716
shannonriv <shannonriv18@gmail.com>
2018-01-15 09:03:02 -0500

    fixing modal post

--- js/pagina5.js
+++ js/pagina5.js
@@ -43,28 +43,34 @@ window.addEventListener('load', function() {
   };
 
   // Función para cargar imágenes
-  function archivo(evt) {
+  function handleFileSelect(evt) {
     var files = evt.target.files; // FileList object
-     
-    // Obtenemos la imagen del campo "file". 
-    for (var i = 0, fgr = files[i]; i++;) {         
-      // Solo admitimos imágenes.
-      if (!fgr.type.match('image.*')) {
+
+    // Loop through the FileList and render image files as thumbnails.
+    for (var i = 0, f; f = files[i]; i++) {
+
+      // Only process image files.
+      if (!f.type.match('image.*')) {
         continue;
       }
-     
-      var reader = new FileReader();
-         
-      reader.onload = (function(theFile) {
-        return function(event) {
-        // Creamos la imagen.
-          document.getElementById('list').innerHTML = ['<img class="thumb" src=""', e.target.result, '" title="', escape(theFile.name), '"/>"'].join('');
-        };
-      })(fgr);
 
-      reader.readAsDataURL(fgr);
+      var reader = new FileReader();
+
+      // Closure to capture the file information.
+      reader.onload = (function(theFile) {
+        return function(e) {
+          // Render thumbnail.
+          var span = document.createElement('span');
+          span.innerHTML = ['<img class="thumb" src="', e.target.result,
+            '" title="', escape(theFile.name), '"/>'].join('');
+          document.getElementById('list').insertBefore(span, null);
+        };
+      })(f);
+
+      // Read in the image file as a data URL.
+      reader.readAsDataURL(f);
     }
   }
-  
-  document.getElementById('files').addEventListener('change', archivo, false);
+
+  document.getElementById('files').addEventListener('change', handleFileSelect, false);
 });
